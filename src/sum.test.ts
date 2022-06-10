1939934b3cb6cea55b46d273ae79f81ec07dc0b0
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-06-10 15:54:31 -0300

    feat: add fail test

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -8,4 +8,8 @@ describe("Math", () => {
   it("Should be able to sum 2 and 3", () => {
     expect(sum(2, 3)).toBe(5);
   });
+
+  it("Should be fail", () => {
+    expect(sum(2, 2)).toBe(5);
+  });
 });
